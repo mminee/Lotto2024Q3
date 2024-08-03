9919ffae6546f8599649d2b4c90c13398148f390
mminee <none@none>
2024-08-03 13:00:58 +0200

    second commit

--- src/main/java/de/mminee/ErrorRateCalculator.java
+++ src/main/java/de/mminee/ErrorRateCalculator.java
@@ -1,17 +1,17 @@
-    package de.mminee;
+package de.mminee;
 
-    import java.util.Set;
+import java.util.Set;
 
-    public class ErrorRateCalculator {
+public class ErrorRateCalculator {
 
-        public double calculateErrorRate (Set<Integer> lastLottoNumbers, Set<Integer> newLottoNumbers){
-            int matches = 0;
+    public double calculateErrorRate(Set<Integer> lastLottoNumbers, Set<Integer> newLottoNumbers) {
+        int matches = 0;
 
-            for (int number : newLottoNumbers) {
-                if (lastLottoNumbers.contains(number)) {
-                    matches++;
-                }
+        for (int number : newLottoNumbers) {
+            if (lastLottoNumbers.contains(number)) {
+                matches++;
             }
-            return (double) matches / newLottoNumbers.size();
         }
+        return (double) matches / newLottoNumbers.size();
     }
+}
